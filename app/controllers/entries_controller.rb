e5b00b9d98547aafb402905ff207c27c101cb6cd
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-03-14 23:04:13 +0000

    user showing in entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -11,6 +11,8 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
+
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ApplicationRecord
   has_one_attached :uploaded_image
+  belongs_to :user
+  belongs_to :place
 end
